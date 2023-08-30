752a99fc0a200f9f4935a265ef9aad173796986c
aravindkumar-thota <84280294+aravindkumar-thota@users.noreply.github.com>
2023-08-30 23:36:32 +0530

    Web service integratrion

---
+++ app.R
@@ -0,0 +1,18 @@
+library(httr)
+library(jsonlite)
+library(shiny)
+base_url <- "https://ctr26perf1.ct-renaissance.com/cdr/outbound/target/project/data/select/table"
+response <- GET(base_url,
+                add_headers('Authorization' = 'XpmogFxpEErj88ohFJQ3ewU6xGLAS4',
+                            'study' = 'Maxis-08',
+                            'project' = 'CDR_Project',
+                            'Table' = 'DM',
+                             'limit' = 10,
+                             'offset' = 0))
+ 
+data <- fromJSON(content(response, "text"))
+DM <- as.data.frame(data)
+print(DM)
+# head(DM)
+# tail(DM)
+View(DM)
