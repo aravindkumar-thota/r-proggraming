d52ca5cc7cc4b611c181074d13a8a708cf49ddc5
aravindkumar-thota <84280294+aravindkumar-thota@users.noreply.github.com>
2023-08-31 17:46:38 +0530

    Update app.R

--- app.R
+++ app.R
@@ -1,18 +1,10 @@
+library(rjson)
 library(httr)
-library(jsonlite)
-library(shiny)
-base_url <- "https://denalisb.ct-renaissance.com//cdr/outbound/target/project/data/select/table"
-response <- GET(base_url,
-                add_headers('Authorization' = 'XpmogFxpEErj88ohFJQ3ewU6xGLAS4',
-                            'study' = 'Maxis-08',
-                            'project' = 'CDR_Project',
-                            'Table' = 'DM',
-                             'limit' = 10,
-                             'offset' = 0))
- 
-data <- fromJSON(content(response, "text"))
-DM <- as.data.frame(data)
-print(DM)
-# head(DM)
-# tail(DM)
-View(DM)
+
+url <- "https://www.zipcodeapi.com/rest/DemoOnly00CS83jqI3dmpepOGDABqGZ5H8B9GNC1WhXOHM5WKuUwgmwu22P4QaaZ/state-zips.json/NJ"
+print(url)
+get_result <- GET(url)
+
+print(get_result)
+#zip_data <- fromJSON(get_result)
+#zip_data
